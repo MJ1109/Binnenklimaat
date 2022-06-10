66637f01ad04175d45c4c5da1502d3846e4def60
Niek Rottier <niekrottier@gmail.com>
2022-06-10 10:40:44 +0200

    Added a video to PiP

--- src/Components/Klimaatjes.jsx
+++ src/Components/Klimaatjes.jsx
@@ -1,7 +1,18 @@
+import React, { useState } from 'react'
+import '../style/klimaatjes.scss'
+import Reactpip from 'react-picture-in-picture'
+
 function Klimaatjes() {
+    const[active, setActive] = useState(false)
 
     return(
-        <div>HELLO IK BEN KLIMAATJES</div>
+        <div>
+            <h1>HELLO IK BEN KLIMAATJES</h1>
+            <Reactpip isActive= {active} controls= {false} autoplay= {true} loop= {true} muted= {true} id='pip-video'>
+                <source src="assets/nyancat-placeholder.mp4"/>
+            </Reactpip>
+            <button onClick = {() => setActive(!active)}>Toggle Picture in Picture</button>
+        </div>
     )
 }
 
