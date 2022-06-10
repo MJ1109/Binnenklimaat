c8c7d797e8aa6174f41322e9122994852f909627
Niek Rottier <niekrottier@gmail.com>
2022-06-10 12:06:08 +0200

    Chooses a random video to play

--- src/Components/Klimaatjes.jsx
+++ src/Components/Klimaatjes.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../style/klimaatjes.scss'
 import Reactpip from 'react-picture-in-picture'
 
 function Klimaatjes() {
-    const[active, setActive] = useState(false)
+    const [active, setActive] = useState(false)
+    const [video, setVideo] = useState("assets/nyancat-placeholder.mp4")
+
+    function pickVideo() {
+        let random = Math.random()
+        if (random > 0.5) {
+            console.log('0.5 < ' + random)
+            setVideo("assets/bobsplant.mp4")
+        }
+    }
+    
+    pickVideo()
 
     return(
         <div>
             <h1>HELLO IK BEN KLIMAATJES</h1>
             <Reactpip isActive= {active} controls= {false} autoplay= {true} loop= {true} muted= {true} id='pip-video'>
-                <source src="assets/nyancat-placeholder.mp4"/>
+                <source src= {video}/>
             </Reactpip>
             <button onClick = {() => setActive(!active)}>Toggle Picture in Picture</button>
         </div>
