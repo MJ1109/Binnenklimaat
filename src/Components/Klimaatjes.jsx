994d32788f67bd60a8ff96f4bf49df22daa35f05
mmerveon <45422341+mmerveon@users.noreply.github.com>
2022-06-17 11:38:16 +0200

    css button veranderd

--- src/Components/Klimaatjes.jsx
+++ src/Components/Klimaatjes.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import '../style/klimaatjes.scss'
 import Reactpip from 'react-picture-in-picture'
 
+
 function Klimaatjes() {
     const [active, setActive] = useState(false)
     const [video, setVideo] = useState("assets/goed co2.mp4")
@@ -13,16 +14,17 @@ function Klimaatjes() {
             setVideo("assets/nog heel even co2.mp4")
         }
     }
-    
+
     pickVideo()
 
     return(
         <div>
-            <h1>HELLO IK BEN KLIMAATJES</h1>
+            <h1>HELLO IK BEN KLIMAATJE</h1>
             <Reactpip isActive= {active} controls= {false} autoplay= {true} loop= {true} muted= {true} id='pip-video'>
                 <source src= {video}/>
             </Reactpip>
-            <button onClick = {() => setActive(!active)}>Toggle Picture in Picture</button>
+            <p></p>
+            <button id='toggle-button' onClick = {() => setActive(!active)}>Toggle Picture in Picture</button>
         </div>
     )
 }
